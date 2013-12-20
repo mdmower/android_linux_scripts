9b7283a75a34912a235070017ac263f6ba827d9b
Matt Mower <mowerm@gmail.com>
2013-12-20 16:23:09 -0600

    last-repo-sync: print both local and UTC times

--- last-repo-sync.sh
+++ last-repo-sync.sh
@@ -4,14 +4,16 @@ CRST=$(tput sgr0)
 RED=$(tput setaf 1)
 YLW=$(tput setaf 3)
 
-if [ ! -d "$ANDROID_BUILD_TOP" ]; then
-	if [ ! -f "./.repo/.repopickle_fetchtimes" ]; then
-		echo "${RED}Error:${CRST} either initialize your android environment or run this"
-		echo "       script from the top of your android tree."
-		exit
-	else
-		echo "${YLW}Last repo sync:${CRST} $(date -d "1970-01-01 + $(stat -c %Z .repo/.repopickle_fetchtimes) secs" -u +"%e %b %Y, %T %Z")"
-	fi
+if [ -d "$ANDROID_BUILD_TOP" ]; then
+	LRSPATH="$ANDROID_BUILD_TOP/.repo/.repopickle_fetchtimes"
+elif [ -f ".repo/.repopickle_fetchtimes" ]; then
+	LRSPATH=".repo/.repopickle_fetchtimes"
 else
-	echo "${YLW}Last repo sync:${CRST} $(date -d "1970-01-01 + $(stat -c %Z $ANDROID_BUILD_TOP/.repo/.repopickle_fetchtimes) secs" -u +"%e %b %Y,  %T %Z")"
+	echo "${RED}Error:${CRST} either initialize your android environment or run this"
+	echo "script from the top of your android tree."
+	exit
 fi
+
+LRSLOCAL=$(date -d "$(stat -c %z $LRSPATH)" +"%e %b %Y, %l:%M:%S %p %Z")
+LRSUTC=$(date -d "$(stat -c %z $LRSPATH)" -u +"%e %b %Y, %T %Z")
+echo "${YLW}Last repo sync:${CRST} $LRSLOCAL / $LRSUTC"
