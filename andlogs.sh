835313a23adb64999135fd93b79cb2b7e46bf75e
Matt Mower <mowerm@gmail.com>
2016-02-18 09:56:22 -0600

    andlogs: Catch sigterm and optionally log radio

--- andlogs.sh
+++ andlogs.sh
@@ -6,13 +6,23 @@ if [ -z "$1" ]; then
     echo "Usage: andlogs.sh basefilename"
     exit 1
 fi
-
 BFNAME="$1"
+
+if [ "$2" == "radio" ]; then
+    RADIO=true
+fi
+
 adb wait-for-device
 adb logcat > "$BFNAME""_lc.log" &
-LCPID=$!
+LOGCAT_PID=$!
+if [ "$RADIO" = true ]; then
+    adb logcat -b radio > "$BFNAME""_rlc.log" &
+    RADIO_PID=$!
+fi
 adb shell 'su -c "cat /proc/kmsg"' > "$BFNAME""_kmsg.log" &
-KMPID=$!
+KMSG_PID=$!
+
+trap "kill $LOGCAT_PID $KMSG_PID $RADIO_PID; exit" SIGINT SIGTERM SIGHUP
 
 echo "Logging started..."
 echo ""
@@ -23,18 +33,19 @@ read -n 1 openlogs
 echo ""
 
 shopt -s nocasematch
-if [[ "$openlogs" == "y" ]]; then
-    kill $LCPID
-    kill $KMPID
+if [[ "$openlogs" == "y" ]] || [[ "$openlogs" == "p" ]]; then
     $LOGVIEWER "$BFNAME""_lc.log" "$BFNAME""_kmsg.log"
-elif [[ "$openlogs" == "p" ]]; then
-    $LOGVIEWER "$BFNAME""_lc.log" "$BFNAME""_kmsg.log"
+    if [ "$RADIO" = true ]; then
+        $LOGVIEWER "$BFNAME""_rlc.log"
+    fi
+fi
+if [[ "$openlogs" == "p" ]]; then
     echo "Press any key to end logging..."
     read -n 1 openlogs
     echo ""
-    kill $LCPID
-    kill $KMPID
-else
-    kill $LCPID
-    kill $KMPID
+fi
+
+kill $LOGCAT_PID $KMSG_PID
+if [ "$RADIO" = true ]; then
+    kill $RADIO_PID
 fi
